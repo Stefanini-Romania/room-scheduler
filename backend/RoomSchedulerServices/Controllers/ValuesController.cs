4dbb3e40a255c14f18a9f9260337902ec17f758e
GeorgeBogdan <lgbogdan12@gmail.com>
2017-10-12 17:55:03 +0300

    Fixed an error

--- backend/RoomSchedulerServices/Controllers/ValuesController.cs
+++ backend/RoomSchedulerServices/Controllers/ValuesController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using RSEntity
+using RSEntity;
 
 namespace RoomSchedulerServices.Controllers
 {
