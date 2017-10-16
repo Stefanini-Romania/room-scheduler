b9f671197c35106ba7e413a9eb16d889bc049385
VeronicaR <veronica.radu@stefanini.com>
2017-10-16 13:12:33 +0300

    Create IRepository.cs, Repository.cs and BaseEntity.cs

---
+++ backend/RSData/Models/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RSData.Models
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ backend/RSRepository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using RSData.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RSRepository
+{
+    interface IRepository<T> where T : BaseEntity
+    {
+        IEnumerable<T> GetAll();
+        T Get(long id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void Remove(T entity);
+        void SaveChanges();
+    }
+}
