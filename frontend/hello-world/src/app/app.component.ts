c0a1f4708bc4800d98315d1927ed9a93d595a487
Paula <paulacjn@yahoo.com>
2017-10-19 17:25:58 +0300

    Revert "Revert "Login component""
    This reverts commit b5a45aaa2924f6b0a08b5f8d938de059ee0f2369.

--- frontend/hello-world/src/app/app.component.ts
+++ frontend/hello-world/src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  providers: [],
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'World';
 
-  authenticate(event) {
-      alert('click');
-  }
+export class AppComponent {
 }
+/*
+class UsersAppComponent { 
+    user = new User();
+}
+*/
---
+++ frontend/hello-world/src/app/models/user.ts
@@ -0,0 +1,9 @@
+export class User {
+    UserName: string;
+    Password: string;
+
+    // constructor(UserName, Password) {
+    //     this.UserName = UserName;
+    //     this.Password = Password;
+    // }
+}
