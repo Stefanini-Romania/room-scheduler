7a664383b22767c48a0d97be2d682bc06912c760
DianaD3 <diana-doina.dinu@stefanini.com>
2017-10-19 16:52:04 +0300

    Login component

--- frontend/hello-world/src/app/app.module.ts
+++ frontend/hello-world/src/app/app.module.ts
@@ -4,12 +4,20 @@ import { RSCalendarModule } from '../calendar/rs-calendar.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule }   from '@angular/forms';
 import { AppComponent } from './app.component';
 import { RSNavbar } from './rs-navbar.component';
-import {RSContent} from './rs-content.component';
-import {RSFooter} from './rs-footer.component';
+import { RSFooter } from './rs-footer.component';
 import {LoginComponent} from './login.component';
 
+// import {FlexLayoutModule} from "@angular/flex-layout";
+// import { routing, appRoutingProviders }  from './app.routing';
+// import { AppServices } from './app.services';
+// import {AuthGuard} from "./auth/auth.guard";
+// import {AppComponent} from './app.component';
+// import {AuthComponent} from './auth/auth.component';
+// import {NotFoundComponent} from './404/not-found.component';
+// import { HomeComponent } from './home/home.component';
 
 
 const appRoutes: Routes = [
@@ -33,18 +41,16 @@ const appRoutes: Routes = [
 
 @NgModule({
   declarations: [
-    AppComponent, RSNavbar, RSContent, RSFooter, LoginComponent
+    AppComponent, RSNavbar, RSFooter, LoginComponent
   ],
   imports: [
     BrowserModule,
     RSCalendarModule,
+    FormsModule,
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
     ),
-    
-    
-    
   ],
   
   providers: [],
