8dfb377eda21b45fdac3351719412e4dcd5d0a8d
GeorgeBogdan <lgbogdan12@gmail.com>
2017-10-19 15:13:16 +0300

    Added TimeSlot Repository

---
+++ backend/RSRepository/ITimeSlotRepository.cs
@@ -0,0 +1,17 @@
+﻿using RSData.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RSRepository
+{
+   public interface ITimeSlotRepository
+    {
+        IEnumerable<TimeSlot> GetTimeSlots();
+        TimeSlot GetTimeSlotById(int id);
+        void AddTimeSlot(TimeSlot _timeslot);
+        void UpdateTimeSlot(TimeSlot _timeslot);
+        void DeleteTimeSlot(TimeSlot _timeslot);
+        void SaveChanges();
+    }
+}
