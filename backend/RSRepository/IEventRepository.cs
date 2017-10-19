bacff41f2c798d9c8900bdbb206f09ecc7695468
jibotean_raul <jibotean_raul@yahoo.com>
2017-10-19 11:54:21 +0300

    Added repository for events

---
+++ backend/RSRepository/IEventRepository.cs
@@ -0,0 +1,17 @@
+﻿using RSData.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RSRepository
+{
+    public interface IEventRepository
+    {
+        IEnumerable<Event> GetEvents();
+        Event GetEventById(int id);
+        void AddEvent(Event _event);
+        void UpdateEvent(Event _event);
+        void DeleteEvent(Event _event);
+        void SaveChanges();
+    }
+}
