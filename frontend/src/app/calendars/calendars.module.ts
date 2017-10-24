187b4d290cd5dd68f995e7db027a549b9fb0e2a1
Madalina Butanche <madalina.butanche@yahoo.com>
2017-10-24 11:30:36 +0300

    Changed Scheduler with a new one

--- frontend/src/app/calendars/calendars.module.ts
+++ frontend/src/app/calendars/calendars.module.ts
@@ -1,15 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CalendarModule, CalendarEventTitleFormatter } from 'angular-calendar';
-import { CustomEventTitleFormatter } from './custom-event-title-formatter.provider';
-import { RSCalendarComponent } from './default/rs-calendar.component';
+import { RSCalendarComponent} from '../calendars/default/rs-calendar.component'
 import { RouterModule, Routes } from '@angular/router';
-import {RSCalendarHeaderComponent} from './default/rs-calendar-header.component';
-import {DateTimePickerComponent} from './shared/date-time-picker.component';
-import {
-    NgbDatepickerModule,
-    NgbTimepickerModule
-} from '@ng-bootstrap/ng-bootstrap';
+import {SchedulerModule} from '../../../node_modules/jqwidgets-framework/demos/angular/app/modules/scheduler.module';
+
 import { FormsModule } from '@angular/forms';
 
 const routes : Routes =[
@@ -21,15 +15,13 @@ const routes : Routes =[
   imports: [
     FormsModule,
     BrowserAnimationsModule,
-    CalendarModule.forRoot(),
-    NgbDatepickerModule.forRoot(),
-    NgbTimepickerModule.forRoot(),
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    SchedulerModule
   ],
   providers: [
-    { provide: CalendarEventTitleFormatter, useClass: CustomEventTitleFormatter}
+    
   ],
-  declarations: [RSCalendarComponent, RSCalendarHeaderComponent, DateTimePickerComponent],
+  declarations: [RSCalendarComponent],
   exports: [RSCalendarComponent],
 })
 export class CalendarsModule {}
--- frontend/src/app/calendars/default/rs-calendar.component.ts
+++ frontend/src/app/calendars/default/rs-calendar.component.ts
@@ -1,47 +1,118 @@
-import { Component, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
-import { CalendarEvent } from 'angular-calendar';
-import { setHours, setMinutes, subDays, addDays } from 'date-fns';
-import { RSCalendarEventColors } from './rs-calendar-event-colors';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { jqxSchedulerComponent } from '../../../../node_modules/jqwidgets-framework/jqwidgets-ts/angular_jqxscheduler';
 
 @Component({
-  selector: 'rs-calendar-component',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  encapsulation: ViewEncapsulation.None,
-  templateUrl: './rs-calendar.component.html'
+  selector: 'app-root',
+  templateUrl: './rs-calendar.component.html',
+//   styleUrls: ['./app.component.css']
 })
-
-export class RSCalendarComponent {
-  view: string = 'day';
-
-  viewDate: Date = new Date();
-
-  // CODED EVENTS
-  events: CalendarEvent[] = [
-    {
-      title: 'Event1',
-      start: new Date(),
-      color: RSCalendarEventColors.blue
-    },
-    {
-      title: 'Event5',
-      start: setHours(setMinutes(new Date(), 30), 9),
-      color: RSCalendarEventColors.yellow
-    }
-  ];
-
-  // EXCLUDE WEEKENDS
-  excludeDays: number[] = [0, 6];
-  skipWeekends(direction: 'back' | 'forward'): void {
-    if (this.view === 'day') {
-      if (direction === 'back') {
-        while (this.excludeDays.indexOf(this.viewDate.getDay()) > -1) {
-          this.viewDate = subDays(this.viewDate, 1);
-        }
-      } else if (direction === 'forward') {
-        while (this.excludeDays.indexOf(this.viewDate.getDay()) > -1) {
-          this.viewDate = addDays(this.viewDate, 1);
-        }
-      }
-    }
+export class RSCalendarComponent implements AfterViewInit {
+  @ViewChild('schedulerReference') scheduler: jqxSchedulerComponent;
+  ngAfterViewInit(): void {
+      this.scheduler.ensureAppointmentVisible('id1');
   }
+
+  generateAppointments(): any {
+      let appointments = new Array();
+      let appointment1 = {
+          id: "id1",
+          description: "George brings projector for presentations.",
+          location: "",
+          subject: "Quarterly Project Review Meeting",
+          calendar: "Room 1",
+          start: new Date(2016, 10, 23, 9, 0, 0),
+          end: new Date(2016, 10, 23, 16, 0, 0)
+      };
+      let appointment2 = {
+          id: "id2",
+          description: "",
+          location: "",
+          subject: "IT Group Mtg.",
+          calendar: "Room 2",
+          start: new Date(2016, 10, 24, 10, 0, 0),
+          end: new Date(2016, 10, 24, 15, 0, 0)
+      };
+      let appointment3 = {
+          id: "id3",
+          description: "",
+          location: "",
+          subject: "Course Social Media",
+          calendar: "Room 3",
+          start: new Date(2016, 10, 27, 11, 0, 0),
+          end: new Date(2016, 10, 27, 13, 0, 0)
+      };
+      let appointment4 = {
+          id: "id4",
+          description: "",
+          location: "",
+          subject: "New Projects Planning",
+          calendar: "Room 2",
+          start: new Date(2016, 10, 23, 16, 0, 0),
+          end: new Date(2016, 10, 23, 18, 0, 0)
+      };
+      let appointment5 = {
+          id: "id5",
+          description: "",
+          location: "",
+          subject: "Interview with James",
+          calendar: "Room 1",
+          start: new Date(2016, 10, 25, 15, 0, 0),
+          end: new Date(2016, 10, 25, 17, 0, 0)
+      };
+      let appointment6 = {
+          id: "id6",
+          description: "",
+          location: "",
+          subject: "Interview with Nancy",
+          calendar: "Room 4",
+          start: new Date(2016, 10, 26, 14, 0, 0),
+          end: new Date(2016, 10, 26, 16, 0, 0)
+      };
+      appointments.push(appointment1);
+      appointments.push(appointment2);
+      appointments.push(appointment3);
+      appointments.push(appointment4);
+      appointments.push(appointment5);
+      appointments.push(appointment6);
+      return appointments;
+  };
+  source: any =
+  {
+      dataType: "array",
+      dataFields: [
+          { name: 'id', type: 'string' },
+          { name: 'description', type: 'string' },
+          { name: 'location', type: 'string' },
+          { name: 'subject', type: 'string' },
+          { name: 'calendar', type: 'string' },
+          { name: 'start', type: 'date' },
+          { name: 'end', type: 'date' }
+      ],
+      id: 'id',
+      localData: this.generateAppointments()
+  };
+  dataAdapter: any = new jqx.dataAdapter(this.source);
+  date: any = new jqx.date(2016, 11, 23);
+  appointmentDataFields: any =
+  {
+      from: "start",
+      to: "end",
+      id: "id",
+      description: "description",
+      location: "location",
+      subject: "subject",
+      resourceId: "calendar"
+  };
+  resources: any =
+  {
+      colorScheme: "scheme05",
+      dataField: "calendar",
+      source: new jqx.dataAdapter(this.source)
+  };
+  views: any[] =
+  [
+      'dayView',
+      'weekView',
+      // 'monthView'
+  ];
 }
