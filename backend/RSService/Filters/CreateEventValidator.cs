5d3221bdfd34c5890f8620e4057ececc68720081
VeronicaR <veronica.radu@stefanini.com>
2017-11-01 10:07:46 +0200

    Fixed CanBook function

--- backend/RSService/Filters/CreateEventValidator.cs
+++ backend/RSService/Filters/CreateEventValidator.cs
@@ -26,13 +26,13 @@ namespace RSService.Filters
                 .NotEmpty().WithMessage("End Date is required")
                 .GreaterThanOrEqualTo(m => m.StartDate.Value).WithMessage("End Date must be greater than Start Date").When(m => m.StartDate.HasValue)
                 .GreaterThanOrEqualTo(m => m.StartDate.Value.AddMinutes(30)).WithMessage("An event must be booked for at least 30 minutes").When(m => m.StartDate.HasValue)
-                .LessThanOrEqualTo(m => m.StartDate.Value.AddHours(1)).WithMessage("An event can be booked for maximum 1 hour").When(m => m.StartDate.HasValue)
-                .Must(CanBook).WithMessage("You can't book more events for this day");
+                .LessThanOrEqualTo(m => m.StartDate.Value.AddHours(1)).WithMessage("An event can be booked for maximum 1 hour").When(m => m.StartDate.HasValue);
+               
         }
 
         private bool CanBook(EventViewModel ev, DateTime? d)
         {
-            return rsManager.GetTimeSpan((DateTime)ev.StartDate, (DateTime)ev.EndDate) >= rsManager.GetAvailableTime(ev.AttendeeId, (DateTime)ev.StartDate);
+            return rsManager.GetTimeSpan((DateTime)ev.StartDate, (DateTime)ev.EndDate) <= rsManager.GetAvailableTime(ev.AttendeeId, (DateTime)ev.StartDate);
         }
     }
 }
