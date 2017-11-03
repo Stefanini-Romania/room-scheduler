a65adc9e748b548dbb0b83056ba44af0a96aeaa7
jibotean_raul <jibotean_raul@yahoo.com>
2017-11-03 12:02:08 +0200

    Modify validation for event

--- backend/RSService/Filters/CreateEventValidator.cs
+++ backend/RSService/Filters/CreateEventValidator.cs
@@ -19,12 +19,13 @@ namespace RSService.Filters
 
             RuleFor(m => m.StartDate)
                 .NotEmpty().WithMessage("Start Date is required")    //.WithErrorCode("EmptyStartDate")
-                .GreaterThanOrEqualTo(DateTime.UtcNow);
+                .GreaterThanOrEqualTo(DateTime.UtcNow)
+                .LessThan(m => m.EndDate.Value).WithMessage("Start Date must be less than End Date").When(m => m.EndDate.HasValue);
             //.Must(CanBook).WithMessage("You can't book more events for this day");
 
             RuleFor(m => m.EndDate)
                 .NotEmpty().WithMessage("End Date is required")
-                .GreaterThanOrEqualTo(m => m.StartDate.Value).WithMessage("End Date must be greater than Start Date").When(m => m.StartDate.HasValue)
+                .GreaterThan(m => m.StartDate.Value).WithMessage("End Date must be greater than Start Date").When(m => m.StartDate.HasValue)
                 .GreaterThanOrEqualTo(m => m.StartDate.Value.AddMinutes(30)).WithMessage("An event must be booked for at least 30 minutes").When(m => m.StartDate.HasValue)
                 .LessThanOrEqualTo(m => m.StartDate.Value.AddHours(1)).WithMessage("An event can be booked for maximum 1 hour").When(m => m.StartDate.HasValue);
                
