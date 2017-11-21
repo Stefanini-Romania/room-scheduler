884bd0756eb3d01e7124eb0fbfa9c4022384debd
Paula <paulacjn@yahoo.com>
2017-11-21 11:56:29 +0200

    Changed color for current user's events

--- frontend/src/app/calendars/default/rs-calendar.component.ts
+++ frontend/src/app/calendars/default/rs-calendar.component.ts
@@ -385,11 +385,13 @@ export class RSCalendarComponent implements OnInit, AfterViewInit, OnDestroy {
         }
         let event = this.events.find(e => e.id == data.appointment.id);
         if (event.eventType == EventTypeEnum.availability ) {
-            data.style = '#E0E0E0'; //gri
-            
-        } else {
-            data.style = '#004e9e'; //albastru
-        }
+            data.style = '#E0E0E0'; //grey
+        } else if (event.attendeeId == this.authService.getLoggedUser().id) {
+            data.style = "#d7dd3b"; //green?
+            } else {
+                data.style = '#004e9e'; //blue
+            }
         return data;
+        
     }
 }
