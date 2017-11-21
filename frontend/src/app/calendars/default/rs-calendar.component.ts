5b05b6b487f8ad2d68cf35c31c3ae0301ca976c2
Paula <paulacjn@yahoo.com>
2017-11-21 12:21:35 +0200

    Fixed event's color

--- frontend/src/app/calendars/default/rs-calendar.component.ts
+++ frontend/src/app/calendars/default/rs-calendar.component.ts
@@ -383,14 +383,20 @@ export class RSCalendarComponent implements OnInit, AfterViewInit, OnDestroy {
         if (!data.appointment) {
             return;
         }
+
         let event = this.events.find(e => e.id == data.appointment.id);
         if (event.eventType == EventTypeEnum.availability ) {
             data.style = '#E0E0E0'; //grey
-        } else if (event.attendeeId == this.authService.getLoggedUser().id) {
-            data.style = "#d7dd3b"; //green?
-            } else {
-                data.style = '#004e9e'; //blue
+        } 
+        else {
+            data.style = '#004e9e'; //blue
+        }
+
+        if ((this.authService.isLoggedIn())) {
+            if (event.attendeeId == this.authService.getLoggedUser().id) {
+                data.style = "#d7dd3b"; //green?
             }
+        }
         return data;
         
     }
