773069143a6ba0f05794667e6d3428b463f0a652
Madalina Butanche <madalina.butanche@yahoo.com>
2017-11-22 13:13:56 +0200

    changed login form in different component

---
+++ frontend/src/app/auth/login/login-form.component.ts
@@ -0,0 +1,31 @@
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+import {User} from '../../shared/models/user.model';
+import {AuthService} from '../shared/auth.service';
+
+@Component({
+    selector: 'login-form',
+    templateUrl: './login-form.component.html',
+    styleUrls: [],
+    providers: [AuthService],
+})
+
+export class LoginFormComponent {
+    public errorMessage: string = '';
+
+    model: User = <User> {};
+
+    constructor(private authService: AuthService, private router: Router) {
+    }
+    login() {
+        this.authService.authenticate(this.model.name, this.model.password)
+            .subscribe(
+                () => {
+                    this.router.navigate(['/calendar']);
+
+                },
+                error => {
+                    this.errorMessage = error.error.message;
+                });
+    }
+}
