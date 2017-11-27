41983c5793cb32d8b54855a4805a631a8f01fe6e
jibotean_raul <jibotean_raul@yahoo.com>
2017-11-27 13:34:48 +0200

    Change penalty information at login - array of roomId

--- backend/RSData/Models/Penalty.cs
+++ backend/RSData/Models/Penalty.cs
@@ -10,7 +10,7 @@ namespace RSData.Models
         public int AttendeeId { get; set; }
         public int EventId { get; set; }
         public DateTime Date { get; set; }
-        public int? RoomId { get; set; }
+        public int RoomId { get; set; }
 
         public User Attendee { get; set; }
         public Event Event { get; set; }
