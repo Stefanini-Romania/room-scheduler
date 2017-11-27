da7ad8b2428a41fd2e787e6bfd38aa954136f6ed
jibotean_raul <jibotean_raul@yahoo.com>
2017-11-27 12:37:29 +0200

    Add new columns in Penalty and User tables. Change RoomPlannerDevContext and appropriate models.

--- backend/RSData/Models/Penalty.cs
+++ backend/RSData/Models/Penalty.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 
 namespace RSData.Models
 {
+
     public partial class Penalty : BaseEntity
     {
+        //public int PenaltyId { get; set; }
         public int AttendeeId { get; set; }
         public int EventId { get; set; }
         public DateTime Date { get; set; }
+        public int? RoomId { get; set; }
 
         public User Attendee { get; set; }
         public Event Event { get; set; }
+        public Room Room { get; set; }
     }
+
 }
