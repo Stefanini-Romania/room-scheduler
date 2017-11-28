31ff353ace5567d544439fe295f6325bdcad4816
VeronicaR <veronica.radu@stefanini.com>
2017-11-28 13:52:26 +0200

    Changed order of fields in UserDTO

--- backend/RSService/DTO/UserDTO.cs
+++ backend/RSService/DTO/UserDTO.cs
@@ -7,10 +7,10 @@ namespace RSService.DTO
 {
     public class UserDTO
     {
-        public string Email { get; set; }
         public string Name { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Email { get; set; }
         public int? DepartmentId { get; set; }
         public List<int> UserRole { get; set; }
     }
