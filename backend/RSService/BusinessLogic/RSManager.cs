50a5c3e49243ce9c2f4ffea8a4899c3897d7cfde
jibotean_raul <jibotean_raul@yahoo.com>
2017-11-29 18:22:17 +0200

    Add Id at CreateAvailabilityEvents.

--- backend/RSService/BusinessLogic/RSManager.cs
+++ backend/RSService/BusinessLogic/RSManager.cs
@@ -37,6 +37,7 @@ namespace RSService.BusinessLogic
 
             DateTime currentDay = startDate.Date;
 
+            int fakeId = 1;
             while (endDate.Date >= currentDay)
             {
                 availabilities = availabilities.Where(e => e.DayOfWeek == (int)currentDay.DayOfWeek).ToList();
@@ -45,6 +46,7 @@ namespace RSService.BusinessLogic
                 {
                     Event newEvent = new Event()
                     {
+                        Id = -fakeId++,
                         StartDate = new DateTime(currentDay.Year, currentDay.Month, currentDay.Day, entry.StartHour.Hour, entry.StartHour.Minute, entry.StartHour.Second),
                         EndDate = new DateTime(currentDay.Year, currentDay.Month, currentDay.Day, entry.EndHour.Hour, entry.EndHour.Minute, entry.EndHour.Second),
                         EventType = 1,
