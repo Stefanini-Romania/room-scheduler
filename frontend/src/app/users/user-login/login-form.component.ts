0868a250a32dafabe26371b0f3e44fd09851d4d1
Madalina Butanche <madalina.butanche@yahoo.com>
2017-12-04 16:51:42 +0200

    list users in admin section

--- frontend/src/app/users/user-login/login-form.component.ts
+++ frontend/src/app/users/user-login/login-form.component.ts
@@ -24,6 +24,7 @@ export class LoginFormComponent {
         this.authService.authenticate(this.model.name, this.model.password)
             .subscribe(
                 () => {
+                    
                 },
                 error => {
                     this.errorMessage = error.error.message;
