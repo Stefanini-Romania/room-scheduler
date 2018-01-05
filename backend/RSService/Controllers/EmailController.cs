c317a12e259d0e3c2c46ed77e9b34f9a3f026152
GeorgeBogdan <lgbogdan12@gmail.com>
2018-01-05 11:38:52 +0200

    Added EmailController.cs  and a method for sending emails

---
+++ backend/RSService/Controllers/EmailController.cs
@@ -0,0 +1,38 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace RSService.Controllers
+{
+    public class EmailController : Controller
+    {
+        [HttpPost("api/email/send")]
+        public IActionResult Send()
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Test Project", "roomchedulerStefanini@gmail.com"));
+            message.To.Add(new MailboxAddress("Scrum Meeting", "lgbogdan12@gmail.com"));
+            message.Subject = "Test email";
+            message.Body = new TextPart("plain")
+            {
+                Text="Hello Room Scheduler"
+            };
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 587, false);
+                client.Authenticate("roomchedulerStefanini@gmail.com", "admin123456");
+
+                client.Send(message);
+
+                client.Disconnect(true);
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ backend/RSService/EmailServices/IEmailService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RSService.EmailServices
+{
+    public interface IEmailService
+    {
+        void Send(EmailMessage emailMessage);
+        List<EmailMessage> ReceiveEmail(int maxCount = 10);
+    }
+}
