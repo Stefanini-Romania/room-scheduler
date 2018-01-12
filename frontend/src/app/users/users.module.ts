949024f98d00cd623c2c24fce3bc338f8e58e266
Paula <paulacjn@yahoo.com>
2018-01-12 18:26:11 +0200

    Added lostPassword component

--- frontend/src/app/users/users.module.ts
+++ frontend/src/app/users/users.module.ts
@@ -11,11 +11,14 @@ import {LoginPageComponent} from './login-form/login-page.component';
 import {LoginFormComponent} from './login-form/login-form.component';
 import {RegisterFormComponent} from './register-form/register-form.component';
 import {RegisterPageComponent} from './register-form/register-page.component';
+import {LostPasswordFormComponent} from './lost-password-form/lost-password-form.component';
+import {LostPasswordPageComponent} from './lost-password-form/lost-password-page.component';
 
 
 const routes: Routes = [
     {path: 'login', component: LoginPageComponent, data:{name: 'login'}},
     {path: 'register', component: RegisterPageComponent, data:{name: 'register'}},
+    {path: 'lostpassword', component: LostPasswordPageComponent, data:{name: 'lostpassword'}}
 ];
 
 @NgModule({
@@ -27,9 +30,9 @@ const routes: Routes = [
         AdminModule
     ],
     providers: [UserService],
-    declarations: [LoginFormComponent, LoginPageComponent, RegisterPageComponent, RegisterFormComponent],
-    exports: [LoginFormComponent, LoginPageComponent, RegisterPageComponent, RegisterFormComponent],
-    entryComponents: [LoginFormComponent, RegisterFormComponent]
+    declarations: [LoginFormComponent, LoginPageComponent, RegisterPageComponent, RegisterFormComponent, LostPasswordFormComponent, LostPasswordPageComponent],
+    exports: [LoginFormComponent, LoginPageComponent, RegisterPageComponent, RegisterFormComponent, LostPasswordFormComponent, LostPasswordPageComponent],
+    entryComponents: [LoginFormComponent, RegisterFormComponent, LostPasswordFormComponent]
 })
 
 export class UsersModule {
