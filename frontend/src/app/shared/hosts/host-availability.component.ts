dc25fd7354a27d1d02078ab913b43b419ed6cd2d
Paula <paulacjn@yahoo.com>
2018-01-31 18:22:17 +0200

    Display partial availability

--- frontend/src/app/shared/hosts/host-availability.component.ts
+++ frontend/src/app/shared/hosts/host-availability.component.ts
@@ -8,6 +8,7 @@ import {EventService} from './../../calendars/shared/event.service';
 import {EventTypeEnum} from '../../shared/models/event.model';
 import {EventStatusEnum} from '../../shared/models/event.model';
 import {HostAvailabilityService} from './../services/host-availability.service';
+import {Availability} from './../models/availability.model';
 //import {HostService} from './../services/host-availability.service';
 
 @Component({
@@ -18,32 +19,63 @@ import {HostAvailabilityService} from './../services/host-availability.service';
 
 export class HostAvailability{
 
-    events: Event[] = [];
     users: User[] = [];
-    availabilities: Event[] = [];
+    public availabilities: Availability[] = [];
+    public model: Availability = <Availability>{};
+    public selectedHost: User;
+    public roomId: number;
     hosts: any[] = [];
     availabilityHostGroupName: string;
     host: User[] = [];
     day: any[] = [];
   
     public startDate: Date;
-    public endDate: Date;
-    
+    public endDate: Date;  
     public hostId: number;
-    private previousValues: any;
 
-    constructor(public HostAvailabilityService: HostAvailabilityService) {
-
-    }
+    constructor(public HostAvailabilityService: HostAvailabilityService) {}
+    
     ngAfterViewInit(){
+        this.listAvailabilities();
+    }
+
+        listAvailabilities()
+    {
+        // if (!this.startDate || !this.endDate || !this.selectedHost) {
+        //     return;
+        // }
+        this.getStartOfWeek();
+        this.startDate = new Date();
+        
+        let hosts = {};
+
         this.availabilities = [];
-        this.HostAvailabilityService.HostAvailabilityList().subscribe((availabilities: any) => {
-            for (let day of availabilities) {
-               
+        this.HostAvailabilityService.HostAvailabilityList(this.model.startDate, this.hostId = 3, this.model.endDate, this.roomId).subscribe((availabilities: Availability[]) => {
+            for (let day of availabilities) {           
+                // if (!hosts[day.hostId]) {
+                //     hosts[day.hostId] = [];
+                // }
+                // hosts[availabilities.].push(availabilities);
                 //for (let event of availabilities[host][day])
-                this.availabilities.push(<Event>availabilities);
+                this.availabilities.push(<Availability>day);
             }
-            //console.log(availabilities);
+            console.log("avails", availabilities);
+            console.log("days", this.day);
         });
     }
+
+    getStartOfWeek(){
+        this.model.startDate = new Date();
+        let dayOfWeek = this.model.startDate.getDay();
+        let currentDate = this.model.startDate.getDate();
+        while(dayOfWeek!==1){
+            dayOfWeek--;
+            currentDate=currentDate-1;
+        }
+        this.model.startDate.setDate(currentDate);
+        this.model.endDate = new Date(this.model.startDate.getFullYear(), this.model.startDate.getMonth(), this.model.startDate.getDate()+4);
+
+    }
+
+    
 }
