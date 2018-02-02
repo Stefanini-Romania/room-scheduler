a14b5d94deecce9b4a5ca31e699dd3873845c88d
Madalina Butanche <madalina.butanche@yahoo.com>
2018-02-02 16:15:23 +0200

    added add availability service

--- frontend/src/app/shared/services/host.service.ts
+++ frontend/src/app/shared/services/host.service.ts
@@ -47,4 +47,26 @@ export class HostService {
     public selectHost(host: User) {
         this.selectedHostChanged$.emit(host);
     }  
+    public AddHostAvailability(startDate: Date, endDate: Date, availabilityType: number, daysOfWeek: any, occurrence: number, roomId?: number){
+        const url = environment.apiUrl + '/availability/add';
+        const body = JSON.stringify({
+            startDate: startDate,
+            endDate: endDate,
+            availabilityType: availabilityType,
+            daysOfWeek: daysOfWeek,
+            occurrence: occurrence,
+            roomId: roomId   
+        });
+        const headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8');
+
+        return this.http.post(url, body, {headers: headers, withCredentials: true})
+            .catch((error: any) => {
+                return Observable.throw(error);
+            })
+            .map((response: Response) => {
+                return response;
+            });
+    }
+
+   
 }
