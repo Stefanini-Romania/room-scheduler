b5e33ee890d96df68c0c45c8afbc93f763c4677d
GeorgeBogdan <lgbogdan12@gmail.com>
2018-02-21 12:34:01 +0200

    Added AuthenticationTests and tests

---
+++ backend/RSTests/AuthenticationTests.cs
@@ -0,0 +1,28 @@
+﻿using RSService.BusinessLogic;
+using RSService.DTO;
+using RSService.Validators;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using System.Linq;
+using RSService.Validation;
+namespace RSTests
+{
+    public class AuthenticationTests
+    {
+        [Fact]
+        public void WhenFields_AreNotFullfield_DenyLogIn()
+        {
+            var rsMoq = new Moq.Mock<IRSBusiness>(Moq.MockBehavior.Loose);
+
+            var validator = new AuthenticationValidator(rsMoq.Object);
+
+            var validationResults = validator.Validate(new CredentialsDto());
+
+            Assert.Equal(1, validationResults.Errors.Count(li => li.ErrorMessage == AuthMessages.EmptyEmail));
+            Assert.Equal(1, validationResults.Errors.Count(li => li.ErrorMessage == AuthMessages.EmptyPassword));
+        }
+
+    }
+}
