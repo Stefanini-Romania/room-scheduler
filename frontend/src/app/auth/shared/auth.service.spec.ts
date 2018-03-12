e0e2250b9a4a094ca3a2853ee806dfe0cb485ffc
Madalina Butanche <madalina.butanche@yahoo.com>
2018-03-12 15:18:32 +0200

    worked on unit testing

--- frontend/src/app/auth/shared/auth.service.spec.ts
+++ frontend/src/app/auth/shared/auth.service.spec.ts
@@ -26,39 +26,51 @@ describe(`AuthService`, () => {
         TranslateModule.forRoot(),
         RouterTestingModule.withRoutes([])
         ],
-      providers: [AuthService, DialogService, NgbModal, MockBackend]
+      providers: [AuthService, DialogService, NgbModal, MockBackend, HttpClient, HttpTestingController]
     });
   });
 
   beforeEach(inject([AuthService, MockBackend], (authService: AuthService, mockBackend: MockBackend) => {
     subject = authService;
     backend = mockBackend;
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000;
     
   }));
 
-  it('login should call endpoint and return it\'s result', () => {
-    backend.connections.subscribe((connection: MockConnection) => {
-      expect(connection.request.method).toEqual(RequestMethod.Post);
-      expect(connection.request.url).toEqual('/login');
-      expect(connection.request.text()).toEqual(JSON.stringify({ username: 'admin@stefanini.com', password: 'admin123456' }));
-      expect(connection.request.headers.get('Content-Type')).toEqual('application/json');
-      let options = new ResponseOptions({
-        body: JSON.stringify({ success: true })
-      });
-      connection.mockRespond(new Response(options));
-    });
+  it(`should send an expected login request`, async(inject([AuthService, HttpTestingController],
+    (auth: AuthService, backend: HttpTestingController) => {
+      auth.authenticate('admin@stefanini.com', 'admin123456').subscribe();
 
-    // subject
-    //   .authenticate( 'admin@stefanini.com', 'admin123456' )
-    //   .subscribe((response) => {
-    //     expect(response.json()).toEqual({ success: true });
-    //     ;
-    //   });
-  })
+      backend.expectOne((req: HttpRequest<any>) => {
+          //const url = environment.apiUrl + '/api/auth/login';
+          const body = JSON.stringify({success: true});
+          // const body = new HttpParams({fromString: req.body});
+
+        return req.url === 'auth/login'        
+          && req.method === 'POST'
+          && req.headers.get('Content-Type') === 'application/json'
+          && req.body.get('loginName') === 'admin@stefanini.com'
+          && req.body.get('password') === 'admin123456';
+      }, `POST to 'auth/login' with loginName and password`);
+  })));
   it('isLoggedIn should return false after creation', inject([AuthService], (service: AuthService) => {
     expect(service.isLoggedIn()).toBeFalsy();
   }));
+
+  it(`should emit 'true' for 200 Ok`, async(inject([AuthService, HttpTestingController],
+    (auth: AuthService, backend: HttpTestingController) => {
+      auth.authenticate('admin@stefanini.com', 'admin123456').subscribe((next) => {
+      backend.expectOne('auth/login').flush({status: 200, statusText: 'Ok'});
+      });
+  })));
+
+  it(`should emit 'false' for 400 Bad Request`, async(inject([AuthService, HttpTestingController],
+    (auth: AuthService, backend: HttpTestingController) => {
+      auth.authenticate('admin@stefanini.com', 'admin123456').subscribe((next) => {
+      backend.expectOne('auth/login').flush({ status: 400, statusText: 'Bad Request' });
+      });
+
+  })));
+
 
   afterEach(inject([HttpTestingController], (backend: HttpTestingController) => {
     backend.verify();
@@ -66,79 +78,10 @@ describe(`AuthService`, () => {
 
 
 
-  // it(`should send an expected login request`, async(inject([AuthService, HttpTestingController],
-  //   (auth: AuthService, backend: HttpTestingController) => {
-  //     auth.authenticate('admin@stefanini.com', 'admin123456').subscribe();
-
-  //     backend.expectOne((req: HttpRequest<any>) => {
-  //         //const url = environment.apiUrl + '/api/auth/login';
-  //         const body = JSON.stringify({success: true});
-  //         // const body = new HttpParams({fromString: req.body});
-
-  //       return req.url === 'auth/login'        
-  //         && req.method === 'POST'
-  //         && req.headers.get('Content-Type') === 'application/json'
-  //         && req.body.get('loginName') === 'admin@stefanini.com'
-  //         && req.body.get('password') === 'admin123456';
-  //     }, `POST to 'auth/login' with loginName and password`);
-  // })));
-
-//   it(`should emit 'false' for 400 Bad Request`, async(inject([AuthService, HttpTestingController],
-//     (auth: AuthService, backend: HttpTestingController) => {
-//       auth.authenticate('admin@stefanini.com', 'admin123456').subscribe((next) => {
-//         expect(next).toBeFalsy();
-//       });
-
-//       backend.expectOne('auth/login').flush(null, { status: 400, statusText: 'Bad Request' });
-//   })));
-
-//   it(`should emit 'true' for 200 Ok`, async(inject([AuthService, HttpTestingController],
-//     (auth: AuthService, backend: HttpTestingController) => {
-//       auth.authenticate('admin@stefanini.com', 'admin123456').subscribe((next) => {
-//         expect(next).toBeTruthy();
-//       });
-
-//       backend.expectOne('auth/login').flush(null, { status: 200, statusText: 'Ok' });
-//   })));
-
  });
 
 
 
 
-// describe('AuthServiceTest', () => {
-//   let subject: AuthService = null;
-//   let backend: MockBackend = null;
-
-//   beforeEach(inject([AuthService, MockBackend], (authService: AuthService, mockBackend: MockBackend) => {
-//     subject = authService;
-//     backend = mockBackend;
-//   }));
-
-//   backend.connections.subscribe((connection: MockConnection) => {
-//     expect(connection.request.method).toEqual(RequestMethod.Post);
-//     expect(connection.request.url).toEqual('/login');
-//     expect(connection.request.text()).toEqual(JSON.stringify({ username: 'admin@stefanini.com', password: 'admin123456' }));
-//     expect(connection.request.headers.get('Content-Type')).toEqual('application/json');
-  
-    
-//   });
 
 
-
-//   it('#login should call endpoint and return it\'s result', (done) => {
-//     backend.connections.subscribe((connection: MockConnection) => {
-//       let options = new ResponseOptions({
-//         body: JSON.stringify({ success: true })
-//       });
-//       connection.mockRespond(new Response(options));
-//     });
-
-//     subject
-//     .authenticate({ loginName: 'admin@stefanini.com', password: 'admin123456' })
-//     .subscribe((response) => {
-//       expect(response.json()).toEqual({ success: true });
-//       done();
-//     });
-//   });
-// });
